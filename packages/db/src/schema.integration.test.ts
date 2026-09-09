7a1afd71d50f4e1edf783d1bc4db6835a114212d
CCA3370 <ryn905010@gmail.com>
2026-09-10 06:55:49 +0800

    test(db): include face diagnostics control-plane table

--- packages/db/src/schema.integration.test.ts
+++ packages/db/src/schema.integration.test.ts
@@ -191,6 +191,7 @@ maybeDescribe("PostgreSQL identity schema", () => {
       "face_album_jobs",
       "face_consent_receipts",
       "face_integration_events",
+      "face_operation_diagnostics",
       "face_search_candidates",
       "face_search_intents",
       "media_face_index_tasks",
@@ -205,6 +206,7 @@ maybeDescribe("PostgreSQL identity schema", () => {
            'face_album_jobs',
            'face_consent_receipts',
            'face_integration_events',
+           'face_operation_diagnostics',
            'face_search_candidates',
            'face_search_intents',
            'media_face_index_tasks'
