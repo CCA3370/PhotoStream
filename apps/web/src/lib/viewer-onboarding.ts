209559a2a6d3fb080fd814c3427bf349775910cd
CCA3370 <ryn905010@gmail.com>
2026-09-14 20:49:09 +0800

    feat: add viewer onboarding state constants

---
+++ apps/web/src/lib/viewer-onboarding.ts
@@ -0,0 +1,7 @@
+export const viewerServiceNoticeStorageKey = "photostream:viewer-service-notice:v1";
+export const viewerServiceNoticeDismissedEvent =
+  "photostream:viewer-service-notice-dismissed";
+
+export const viewerOnboardingStorageKey = "photostream:viewer-onboarding:v1";
+export const viewerLightboxOnboardingStorageKey =
+  "photostream:viewer-lightbox-onboarding:v1";
