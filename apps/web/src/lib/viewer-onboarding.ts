fd5b6946fe1e467822606563387c9cda8f19be1e
CCA3370 <ryn905010@gmail.com>
2026-09-19 17:38:51 +0800

    Bump viewer notice acknowledgement version

--- apps/web/src/lib/viewer-onboarding.ts
+++ apps/web/src/lib/viewer-onboarding.ts
@@ -1,4 +1,4 @@
-export const viewerServiceNoticeStorageKey = "photostream:viewer-service-notice:v3";
+export const viewerServiceNoticeStorageKey = "photostream:viewer-service-notice:v4";
 export const viewerServiceNoticeDismissedEvent = "photostream:viewer-service-notice-dismissed";
 
 export const viewerOnboardingStorageKey = "photostream:viewer-onboarding:v1";
