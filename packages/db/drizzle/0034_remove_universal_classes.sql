c9b1e4d2881fae7a867d1f972480e8c0aea54652
CCA3370 <ryn905010@gmail.com>
2026-09-24 00:23:09 +0800

    remove legacy universal classes

---
+++ packages/db/drizzle/0034_remove_universal_classes.sql
@@ -0,0 +1,49 @@
+WITH invalid_classes AS (
+  SELECT class_option.id, class_option.album_id
+  FROM "bib_attribute_options" AS class_option
+  LEFT JOIN "bib_attribute_options" AS grade_option
+    ON grade_option.id = class_option.parent_grade_option_id
+    AND grade_option.album_id = class_option.album_id
+    AND grade_option.dimension = 'grade'
+  WHERE class_option.dimension = 'class'
+    AND grade_option.id IS NULL
+)
+UPDATE "albums"
+SET
+  "bib_mapping_version" = "bib_mapping_version" + 1,
+  "updated_at" = now()
+WHERE "id" IN (SELECT DISTINCT "album_id" FROM invalid_classes);
+--> statement-breakpoint
+WITH invalid_classes AS (
+  SELECT class_option.id
+  FROM "bib_attribute_options" AS class_option
+  LEFT JOIN "bib_attribute_options" AS grade_option
+    ON grade_option.id = class_option.parent_grade_option_id
+    AND grade_option.album_id = class_option.album_id
+    AND grade_option.dimension = 'grade'
+  WHERE class_option.dimension = 'class'
+    AND grade_option.id IS NULL
+)
+DELETE FROM "bib_attribute_mappings"
+WHERE "output_option_id" IN (SELECT "id" FROM invalid_classes);
+--> statement-breakpoint
+WITH invalid_classes AS (
+  SELECT class_option.id
+  FROM "bib_attribute_options" AS class_option
+  LEFT JOIN "bib_attribute_options" AS grade_option
+    ON grade_option.id = class_option.parent_grade_option_id
+    AND grade_option.album_id = class_option.album_id
+    AND grade_option.dimension = 'grade'
+  WHERE class_option.dimension = 'class'
+    AND grade_option.id IS NULL
+)
+DELETE FROM "bib_attribute_options"
+WHERE "id" IN (SELECT "id" FROM invalid_classes);
+--> statement-breakpoint
+ALTER TABLE "bib_attribute_options" DROP CONSTRAINT IF EXISTS "bib_attribute_options_grade_parent_check";
+--> statement-breakpoint
+ALTER TABLE "bib_attribute_options" ADD CONSTRAINT "bib_attribute_options_hierarchy_check" CHECK (
+  ("dimension" = 'grade' AND "parent_grade_option_id" IS NULL)
+  OR
+  ("dimension" = 'class' AND "parent_grade_option_id" IS NOT NULL)
+);
